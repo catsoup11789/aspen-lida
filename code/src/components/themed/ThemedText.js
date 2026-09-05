3bb4a25875822cf6e5938b1f2d253d9d38620df8
kirstien <kirstienkroeger@icloud.com>
2026-09-04 20:12:41 -0500

    DIS-2810: updated ThemedText sizing

--- code/src/components/themed/ThemedText.js
+++ code/src/components/themed/ThemedText.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Text } from '@/components/ui/text';
 import { useTheme } from '../../themes/theme';
 
-export const ThemedText = React.forwardRef(({ style, ...props }, ref) => {
+export const ThemedText = React.forwardRef(({ size = 'sm', style, ...props }, ref) => {
      const { textColor } = useTheme();
 
-     return <Text ref={ref} style={[{ color: textColor }, style]} {...props} />;
+     return <Text ref={ref} size={size} style={[{ color: textColor }, style]} {...props} />;
 });
 
 ThemedText.displayName = 'ThemedText';
